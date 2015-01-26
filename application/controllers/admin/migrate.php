4ecef614be40233dfc528ee73015b4e0eff9e350
ReZaBiDaR <rezabidar@live.com>
2015-01-26 08:26:21 +0330

    first commit with some useful config like db and one test migration and controler

---
+++ application/controllers/admin/migrate.php
@@ -0,0 +1,13 @@
+<?php
+class Migrate extends CI_Controller{
+    function index(){
+        $this->load->library('migration');
+        
+        if ( ! $this->migration->current())
+        {
+            show_error($this->migration->error_string());
+        }else {
+            echo "db is up to date" ;
+        }
+    }    
+}
