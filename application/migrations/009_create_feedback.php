246d4d6fe55761216dd6280fba4971262d0151b6
ReZaBiDaR <rezabidar@live.com>
2015-02-14 09:28:26 +0330

    change user_id to wiw_id and add creater modifier id

--- application/migrations/009_create_feedback.php
+++ application/migrations/009_create_feedback.php
@@ -15,14 +15,19 @@ class Migration_Create_Feedback extends CI_Migration {
             {$prefix}_send_time DATETIME NULL ,
             {$prefix}_type INT(4) DEFAULT 0 ,
             {$prefix}_task_id INT(14) UNSIGNED NOT NULL ,
-            {$prefix}_user_id INT(14) UNSIGNED NOT NULL ,
+            {$prefix}_wiw_id INT(14) UNSIGNED NOT NULL ,
+            {$prefix}_creator_id INT(14) UNSIGNED NOT NULL ,
             {$prefix}_creator_ip VARCHAR(15) NOT NULL ,
             {$prefix}_created_time DATETIME NOT NULL ,
+            {$prefix}_modifier_id INT(14) UNSIGNED NOT NULL ,
             {$prefix}_modifier_ip VARCHAR(15) NOT NULL ,
             {$prefix}_modified_time DATETIME NOT NULL ,
             CONSTRAINT feedback_pk PRIMARY KEY ({$prefix}_id),
-            CONSTRAINT feedback_fk_user FOREIGN KEY ({$prefix}_user_id) REFERENCES {$this->db->dbprefix("user")} (usr_id) ON DELETE CASCADE ON UPDATE CASCADE ,
-            CONSTRAINT feedback_fk_task FOREIGN KEY ({$prefix}_task_id) REFERENCES {$this->db->dbprefix("task")} (tsk_id) ON DELETE CASCADE ON UPDATE CASCADE  
+            CONSTRAINT feedback_fk_who_is_where FOREIGN KEY ({$prefix}_wiw_id) REFERENCES {$this->db->dbprefix("who_is_where")} (wiw_id) ON DELETE CASCADE ON UPDATE CASCADE ,
+            CONSTRAINT feedback_fk_task FOREIGN KEY ({$prefix}_task_id) REFERENCES {$this->db->dbprefix("task")} (tsk_id) ON DELETE CASCADE ON UPDATE CASCADE ,
+            CONSTRAINT feedback_fk_user_creator FOREIGN KEY ({$prefix}_creator_id) REFERENCES {$this->db->dbprefix("user")} (usr_id) ON DELETE RESTRICT ON UPDATE CASCADE ,
+            CONSTRAINT feedback_fk_user_modifier FOREIGN KEY ({$prefix}_modifier_id) REFERENCES {$this->db->dbprefix("user")} (usr_id) ON DELETE RESTRICT ON UPDATE CASCADE 
+             
                 
             ) ENGINE=INNODB
             DEFAULT CHARSET = utf8
